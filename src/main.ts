b18c765a77fd8801b0605ffca05121fbc5fb4863
Sebastian <sebastianwuczkowski@gmail.com>
2023-09-04 14:27:47 +0200

    add vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
+import { loadFonts } from './plugins/webfontloader'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
-app.use(createPinia())
-app.use(router)
+loadFonts()
 
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .use(vuetify)
+  .mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <main>
-  </main>
+  <hello-world />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+// Components
+import HelloWorld from '../components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    HelloWorld,
+  },
+});
+</script>
