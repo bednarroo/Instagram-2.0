3e872942c468d480b41745c7fe704c9ab4a5a734
Sebastian <sebastianwuczkowski@gmail.com>
2023-09-08 15:01:15 +0200

    login and sign in component

--- src/components/LoginSignin.vue
+++ src/components/LoginSignin.vue
@@ -9,58 +9,35 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">{{ showTitle }}</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <div class="d-flex px-5 " role="group" aria-label="Basic example">
-          <button type="button" class="btn btn-primary">LogIn</button>
-          <button type="button" class="btn btn-primary">Sigin</button>
+        <div class="d-flex px-5 justify-content-center"  role="group" aria-label="Basic example">
+          <button type="button" @click="changeOptionAuth('Login')" class="btn mx-3" :class="showSigninOption === true ? 'btn-primary' : 'btn-secondary'" >Log in</button>
+          <button type="button" @click="changeOptionAuth('Signin')" class="btn mx-3" :class="showSigninOption === true ? 'btn-secondary' : 'btn-primary' " >Sign in</button>
         </div>
         <div>
           <form>
             <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              <label for="email" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+              <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
             </div>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            <div class="mb-3" v-if="showSigninOption">
+              <label for="login" class="form-label">Login</label>
+              <input type="email" class="form-control" id="login" aria-describedby="emailHelp">
+              <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
             </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1">
+            <div class="mb-3" >
+              <label for="password" class="form-label">Password</label>
+              <input type="password" class="form-control" id="password">
             </div>
-            <div class="mb-3 form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1">
-              <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            <div class="d-flex flex-end justify-content-end">
+              <button type="submit" class="btn btn-primary">{{showTitle}}</button>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
           </form>
         </div>
-        <div>
-          <form>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3 form-check">
-              <input type="checkbox" class="form-check-input" id="exampleCheck1">
-              <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-          </form>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
       </div>
     </div>
   </div>
@@ -68,7 +45,26 @@
 </template>
 
 <script setup lang="ts">
+import {ref, computed} from 'vue'
 
+const showSigninOption = ref(true)
+const loginSigninDetial = ref({
+  login: "",
+  email: "",
+  password: ""
+})
+
+const changeOptionAuth = (option) => {
+  if(option === 'Login' ) {
+    return showSigninOption.value = true
+  } else{
+    return showSigninOption.value = false
+  }
+}
+
+const showTitle = computed( () => {
+  return showSigninOption.value  === false ? "Sign in" : "Log in"
+})
 
 </script>
 
