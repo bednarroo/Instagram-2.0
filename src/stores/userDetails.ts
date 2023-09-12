27ba1fd7f8930dcfd463a61886c127057392b603
Sebastian <sebastianwuczkowski@gmail.com>
2023-09-12 17:09:13 +0200

    work on login and sign in

--- src/stores/userDetails.ts
+++ src/stores/userDetails.ts
@@ -2,16 +2,17 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('userDetails', {
   state: () => ({
-    userDetails: {
-      id: null,
-      login: null,
-      email: null,
-    },
+    userDetails: null
+    //  {
+    //   id: null,
+    //   login: null,
+    //   email: null,
+    // },
   }),
-  // actions: {
-  //   logIn(logIn: object) {
-  //     this.userDetails = logIn
-  //   },
+  actions: {
+    logIn(logIn) {
+      this.userDetails = logIn
+    },
   //   // logOut() {
   //   //   this.count++
   //   // }
