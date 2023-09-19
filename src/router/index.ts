493756a6ac6448d786a26d666825aa4169a1bf49
Sebastian <sebastianwuczkowski@gmail.com>
2023-09-19 19:00:42 +0200

    add 404 route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import BoardView from '../views/BoardView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import AboutView from '../views/AboutView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView
+    },
+    {
+      path: '/:notFound',
+      name: 'Not found',
+      component: NotFoundView,
     },
   ]
 })
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-flex  m-5 justify-content-center">
+    
+            <img class="mx-3 my-auto" style="max-width: 800px;" src="../assets/notFound.png" alt="Instagram" >
+            </div>
+            <h1 class=" fw-bolder my-4 text-center">Not found 404 - please go back</h1>
+          
+          <div class="d-flex justify-content-center">
+            <router-link  to="/" >
+            <button class="btn btn-primary"> Go back</button>
+          </router-link>
+          </div>
+          
+</template>
+
+<script lang="ts" setup>
+
+</script>
