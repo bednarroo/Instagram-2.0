ebbd45d0b8ab73c2d2ed0c2136c37593a5869f74
Sebastian <sebastianwuczkowski@gmail.com>
2023-09-20 16:59:38 +0200

    add LoadingSpiiner

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@
   <NavBar />
   <RouterView />
   <Footer />
-  
+  <LoadingSpinner/>
+
 </template>
 
 <script lang="ts" setup>
 import {RouterView} from 'vue-router'
 import NavBar from './components/NavBar.vue'
 import Footer from './components/Footer.vue'
+import LoadingSpinner from './components/LoadingSpinner.vue'
 import { onMounted } from 'vue'
 import {useUserStore} from './stores/userDetails.ts'
 
---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="d-flex flex-column position-fixed w-100 h-100 top-0 start-0 justify-content-center align-items-center" style="background-color: #ff00f863; overflow: hidden">
+            <div class="spinner-border my-5 d-flex flex-column flex-wrap justify-content-center align-content-center " role="status">
+                <img src="../assets/Instagram.webp" style="width: 25vw" >
+            </div>
+            <span class="sr-only h1 my-5">Loading...</span>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+</style>
