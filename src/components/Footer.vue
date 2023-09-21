0bcd2815cf5ba68a5c5c55d8998522e657dcb1e3
Sebastian <sebastianwuczkowski@gmail.com>
2023-09-21 21:47:05 +0200

    cache cleaning

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,12 +7,12 @@
     <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
       <li class="ms-3">
         <a class="text-muted" href="https://github.com/bednarroo">
-            <img src="@/assets/GitHub.png" height="40px" alt="">
+            <!-- <img src="@/assets/GitHub.png" height="40px" alt=""> -->
         </a>
       </li>
       <li class="ms-3">
         <a class="text-muted" href="https://www.linkedin.com/in/sebastianwuczkowski">
-            <img src="@/assets/Linkedin.webp" height="40px" alt="">
+            <!-- <img src="@/assets/Linkedin.webp" height="40px" alt=""> -->
         </a>
       </li>
     
