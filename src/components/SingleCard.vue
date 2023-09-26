1fb9eac84baa18fcaf2c01f11b425d271d86cad9
Sebastian <sebastianwuczkowski@gmail.com>
2023-09-26 22:03:59 +0200

    Edit: card looks

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -5,6 +5,12 @@
       <h4 class="card-title">Card title</h4>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
       <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      <p class="card-text">
+        <small class="text-muted">
+          Added by: Sebastian
+          <img style="width: 20px;" src="../assets/Instagram.webp" class="card-img-top" alt="...">
+        </small>
+      </p>
     </div>
   </div>
 </template>
