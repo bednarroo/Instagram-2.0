661f7b077e62bd66a3360c3251462c35074a6f3a
Sebastian <sebastianwuczkowski@gmail.com>
2023-09-29 09:19:03 +0200

    feature: add follow button

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="d-flex flex-column">
     <AccountStats />
-    <button type="button" class="btn fw-bolder btn-follow">Follow</button>
-    <AddPicture />
+    <button v-if="userStore.userDetails.id" type="button" class="btn fw-bolder btn-follow">
+      Follow
+    </button>
+    <AddPicture v-if="userStore.userDetails.id" />
     <CardContainer />
   </div>
 </template>
@@ -11,6 +13,9 @@
 import AccountStats from '../components/AccountStats.vue'
 import AddPicture from '../components/AddPicture.vue'
 import CardContainer from '../components/CardContainer.vue'
+import {useUserStore} from '../stores/userDetails.ts'
+
+const userStore = useUserStore()
 
 </script>
 
