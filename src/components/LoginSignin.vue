6193b1157fa105997da618f6643d0b2b16c9e191
Sebastian <sebastianwuczkowski@gmail.com>
2023-09-29 09:42:52 +0200

    feature: show error on loggin

--- src/components/LoginSignin.vue
+++ src/components/LoginSignin.vue
@@ -46,9 +46,9 @@
             </div>
           </form>
         </div>
-        <!-- <div v-if="errorLogInSignIn">
-          
-        </div> -->
+        <div v-if="errorLogInSignIn">
+          {{  errorLogInSignIn}}
+        </div>
       </div>
     </div>
   </div>
