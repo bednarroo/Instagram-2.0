5f05bdfa616518bd4f6e19a199a71846767380ac
Sebastian <sebastianwuczkowski@gmail.com>
2023-10-09 20:38:00 +0200

    feature - add followUser function

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex flex-column">
     <AccountStats />
-    <button @click="followUser" v-if="userStore.userDetails.id && userStore.userDetails.login" type="button" class="btn fw-bolder btn-follow">
+    <button @click="followUser" v-if="userStore.userDetails.id && userStore.userDetails.login !== routerId " type="button" class="btn fw-bolder btn-follow">
       Follow
     </button>
     <AddPicture v-if="userStore.userDetails.id" />
@@ -10,12 +10,24 @@
 </template>
 
 <script lang="ts" setup>
+import { onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 import AccountStats from '../components/AccountStats.vue'
 import AddPicture from '../components/AddPicture.vue'
 import CardContainer from '../components/CardContainer.vue'
 import {useUserStore} from '../stores/userDetails.ts'
+import {supabase} from '../dataBase/index'
 
+
+const route = useRoute();
+const routerId = route.params.id
 const userStore = useUserStore()
+
+const followUser = async () => {
+  const { error } = await supabase
+  .from('subscription')
+  .insert({ follower: userStore.userDetails.userId, following: route.params.id  })
+}
 
 </script>
 
