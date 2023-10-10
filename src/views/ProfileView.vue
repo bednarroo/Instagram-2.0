56eba61adccec2e0991e2b6c026275badf63d4c7
Sebastian <sebastianwuczkowski@gmail.com>
2023-10-10 21:35:13 +0200

    feature: add follow option

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="d-flex flex-column">
     <AccountStats />
-    <button @click="followUser" v-if="userStore.userDetails.id && userStore.userDetails.login !== routerId " type="button" class="btn fw-bolder btn-follow">
-      Follow
+    <button @click="folloUnFollowUser" v-if="userStore.userDetails.id && userStore.userDetails.login !== routerId " type="button" class="btn fw-bolder btn-follow">
+      {{followingStatus}}
     </button>
     <AddPicture v-if="userStore.userDetails.id" />
     <CardContainer />
@@ -10,7 +10,7 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from 'vue'
+import { onMounted, computed, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import AccountStats from '../components/AccountStats.vue'
 import AddPicture from '../components/AddPicture.vue'
@@ -22,25 +22,42 @@ import {supabase} from '../dataBase/index'
 const route = useRoute();
 const routerId = route.params.id
 const userStore = useUserStore()
-const isUserFollowed = false;
+const isUserFollowed = ref(false);
 
-const followUser = async () => {
-  const { error } = await supabase
-  .from('subscription')
-  .insert({ follower: userStore.userDetails.login, following: route.params.id  })
+const folloUnFollowUser = async () => {
+  if(isUserFollowed.value === false){
+    const { error } = await supabase
+    .from('subscription')
+    .insert({ follower: userStore.userDetails.login, following: route.params.id  })
+    if(!error){
+      isUserFollowed.value = true
+    }
+  }
 }
+
+const followingStatus = computed(()=> {
+ if(isUserFollowed.value === true){
+  return 'unfollow'
+ }else{
+  return 'follow'
+ }
+})
 const checkIfUserIsFollowed = async () => {
     const { data, error } = await supabase
     .from('subscription')
     .select()
-    .eq('follower', userStore.userDetails.login)
     .eq('following', route.params.id)
-    if(data.length > 0){
-      isUserFollowed.value = true;
-    }
+    .eq('follower', userStore.userDetails.login)
+    // if(data.length > 0){
+    //   isUserFollowed.value = true;
+    // }
+    console.log(data)
+    console.log(userStore.userDetails.login)
+    console.log(route.params.id)
   } 
 
 onMounted(() => {
+  followUser()
   checkIfUserIsFollowed()
 })
 
