73a066b493db4b38f0ec2f2527efcf4d141ae7fb
Sebastian <sebastianwuczkowski@gmail.com>
2023-10-12 20:24:15 +0200

    add styles to AlerLogin componten

--- src/components/AlertLogIn.vue
+++ src/components/AlertLogIn.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <h1 class="display-4">Hello!</h1>
+  <p class="lead">You're not loged in. Please log in to see more without any limits!</p>
   <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
+  <p>This is {{ reminderNumber }} reminder. When you will see third you won't be able to see anything.</p>
+  <div class="lead">
+    I understand
+  </div>
 </div>
 </template>
 
 <script setup lang="ts">
+import {ref} from 'vue'
+
+const reminderNumber = ref(0)
 </script>
 
 <style>
