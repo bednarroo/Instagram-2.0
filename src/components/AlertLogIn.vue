91e2f3fa934faa204a0c1a2ce8f5048bac4c19b5
Sebastian <sebastianwuczkowski@gmail.com>
2023-10-12 20:37:50 +0200

    feature: add Functionality to AlertLogin component

--- src/components/AlertLogIn.vue
+++ src/components/AlertLogIn.vue
@@ -1,10 +1,10 @@
-<template>
+<template v-if="reminderNumber < 3 && !userStore.userDetails.id && showAlertLogin ">
   <div class="jumbotron">
   <h1 class="display-4">Hello!</h1>
   <p class="lead">You're not loged in. Please log in to see more without any limits!</p>
   <hr class="my-4">
   <p>This is {{ reminderNumber }} reminder. When you will see third you won't be able to see anything.</p>
-  <button  class="lead">
+  <button @click="!showAlertLogin"  class="lead" v-if="reminderNumber < 3">
     I understand
   </button>
 </div>
@@ -12,9 +12,24 @@
 
 <script setup lang="ts">
 import {ref, onMounted} from 'vue'
+import {useUserStore} from '../stores/userDetails.ts'
 
+
+const userStore = useUserStore()
 const reminderNumber = ref(0)
-const myInterval = setInterval(showAlerLogin, 1000);
+const showAlertLogin = ref(false)
+const myInterval = setInterval(changeAlertLoginStatus, 10000);
+
+const changeAlertLoginStatus = () => {
+  if(reminderNumber.value < 3){
+    clearInterval(myInterval)
+    myInterval()
+    reminderNumber.value++
+    showAlertLogin.value = false
+  }else{
+    clearInterval(myInterval)
+  }
+}
 
 
 </script>
