5cd3170c3f5c649e6f9f826c3c8cd9a19af9c706
Sebastian <sebastianwuczkowski@gmail.com>
2023-10-12 20:28:07 +0200

    add styles to AlerLogin componten

--- src/components/AlertLogIn.vue
+++ src/components/AlertLogIn.vue
@@ -4,16 +4,19 @@
   <p class="lead">You're not loged in. Please log in to see more without any limits!</p>
   <hr class="my-4">
   <p>This is {{ reminderNumber }} reminder. When you will see third you won't be able to see anything.</p>
-  <div class="lead">
+  <button  class="lead">
     I understand
-  </div>
+  </button>
 </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue'
+import {ref, onMounted} from 'vue'
 
 const reminderNumber = ref(0)
+const myInterval = setInterval(showAlerLogin, 1000);
+
+
 </script>
 
 <style>
