d8b46af2585a6949ad6dd2e0095817bf38ffcdc9
Sebastian <sebastianwuczkowski@gmail.com>
2023-10-16 19:15:12 +0200

    feature: handle close pop-up

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <RouterView />
   <Footer />
   <LoadingSpinner/>
-  <AlertLogIn @hideAlertLogIn="handleClosePopUp" v-if="showAlertLogin !== false"/>
+  <AlertLogIn @hideAlertLogIn="handleClosePopUp" v-if="showAlertLogin !== false" :showCloseButton = "showCloseButton" :reminderNumber="reminderNumber" />
 
 </template>
 
@@ -17,16 +17,21 @@ import { onMounted, ref } from 'vue'
 import {useUserStore} from './stores/userDetails.ts'
 
 const handleClosePopUp = (flow) => {
-  console.log(showAlertLogin.value)
-  clearInterval(myInterval)
-  myInterval = setInterval(showModal, 10000);
-  showAlertLogin.value = false;
+  if(showAlertLogin.value > 3) {
+    showCloseButton.value = false
+    clearInterval(myInterval)
+  } else if (showAlertLogin.value <= 3) {
+    clearInterval(myInterval)
+    myInterval = setInterval(showModal, 10000);
+    showAlertLogin.value = true;
+  }
 }
 
 
 const userStore = useUserStore()
 const reminderNumber = ref(0)
 const showAlertLogin = ref(false)
+const showCloseButton = ref(false)
 let myInterval = null
 
 onMounted (() => {
--- src/components/AlertLogIn.vue
+++ src/components/AlertLogIn.vue
@@ -3,8 +3,8 @@
   <h1 class="display-4">Hello!</h1>
   <p class="lead">You're not loged in. Please log in to see more without any limits!</p>
   <hr class="my-4">
-  <p>This is {{ reminderNumber }} reminder. When you will see third you won't be able to see anything.</p>
-  <button @click="$emit('hideAlertLogIn')" class="lead" v-if="reminderNumber < 3">
+  <p>This is {{ props.sreminderNumber }} reminder. When you will see third you won't be able to see anything.</p>
+  <button @click="$emit('hideAlertLogIn')" class="lead" v-if="showCloseButton">
     I understand
   </button>
 </div>
@@ -16,26 +16,20 @@ import {useUserStore} from '../stores/userDetails.ts'
 
 
 const userStore = useUserStore()
-const reminderNumber = ref(0)
 const showAlertLogin = ref(false)
 let myInterval = null
 
-const emit = defineEmits(['hideAlertLogIn'])
+const props = defineProps({
+  showCloseButton: Boolean,
+  reminderNumber: Number
+})
 
-onMounted(()=> {
-  if(useUserStore?.userDetails?.id === null){
-   myInterval = setInterval(showModal, 10000);
-  }
-});
+const emit = defineEmits(['hideAlertLogIn'])
 
 const closeModal = () => {
   showAlertLogin.value = false
   clearInterval(myInterval)
   myInterval = setInterval(showModal, 10000);
-}
-
-const showModal = () => {
- showAlertLogin.value = true;
 }
 </script>
 
