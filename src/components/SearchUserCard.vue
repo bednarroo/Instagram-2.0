89479fbec5e3f9611d8dd08482b737899ba3fd81
Sebastian <sebastianwuczkowski@gmail.com>
2023-10-23 20:28:04 +0200

    feature: add styles

--- src/components/SearchUserCard.vue
+++ src/components/SearchUserCard.vue
@@ -1,11 +1,13 @@
 !<template>
-    <router-link :to="`/profile/${record.login}`">
+    <router-link class="text-decoration-none" :to="`/profile/${record.login}`">
         <div class="card m-2 d-flex flex-row">
             <div class="p-4">
                 <img src="../assets/Instagram.webp" width="50" alt="">
             </div>
             <div class="card-body d-flex align-items-center">
-                {{ record.login }}
+                <h4 >
+                    {{ record.login }}
+                </h4>
             </div>
         </div>
     </router-link>
