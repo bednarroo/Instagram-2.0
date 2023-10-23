6725a791f1704cf17ef292ca1def34095ab4b108
Sebastian <sebastianwuczkowski@gmail.com>
2023-10-23 20:21:23 +0200

    feature: add routes

--- src/components/SearchUserCard.vue
+++ src/components/SearchUserCard.vue
@@ -1,5 +1,5 @@
 !<template>
-    <router-link to="`/profile/${record.login}`">
+    <router-link :to="`/profile/${record.login}`">
         <div class="card my-1 d-flex flex-row">
             <div class="p-4">
                 <img src="../assets/Instagram.webp" width="50" alt="">
