0fa06e79db2c18df510554ad6b04aab50a62c38a
Sebastian <sebastianwuczkowski@gmail.com>
2023-10-23 19:01:24 +0200

    feature: add styles to SearchProfile component

--- src/components/SearchProfile.vue
+++ src/components/SearchProfile.vue
@@ -2,24 +2,24 @@
   <div>
     <!-- Button trigger modal -->
     
-    <button type="button" class="btn " data-bs-toggle="modal" data-bs-target="#exampleModal">
+    <button type="button" class="btn " data-bs-toggle="modal" data-bs-target="#searchModal">
       
       <img src="../assets/search.svg" alt="" style="width: 15px; margin-right: 10px;" >
       <span style="font-size: 18px;"> Search </span>
     </button>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal" id="searchModal" tabindex="-1" aria-labelledby="searchModal" aria-hidden="true">
   <div class="modal-dialog modal-fullscreen">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Search user:</h5>
+        <h5  class="modal-title text-dark" id="searchModalLabel">Search user:</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <div class="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">Username</label>
-          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+          <label for="exampleFormControlInput1" class="form-label text-dark">Username</label>
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Please type user nick here!">
         </div>
       </div>
       <div class="modal-footer">
