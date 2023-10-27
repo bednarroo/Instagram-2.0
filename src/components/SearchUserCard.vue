bcf89bc158eadaec0002f2d7fd226c7e65ae7b0c
Sebastian <sebastianwuczkowski@gmail.com>
2023-10-27 16:33:28 +0200

    feature: add styles to Search Component

--- src/components/SearchUserCard.vue
+++ src/components/SearchUserCard.vue
@@ -1,8 +1,8 @@
 !<template>
     <router-link @click="closeSearchModal" class="text-decoration-none" :to="`/profile/${record.login}`">
         <div class="card m-2 d-flex flex-row">
-            <div class="p-4">
-                <img :src='record.main_photo' width="50" alt="">
+            <div class="p-1 m-2">
+                <img class="m-1" :src='record.main_photo' width="50" alt="">
             </div>
             <div class="card-body d-flex align-items-center">
                 <h4 >
@@ -16,7 +16,7 @@
 <script setup>
 import {Modal} from 'bootstrap'
 
-let myModal = new Modal(document.getElementById('searchModal'));
+let myModal = new Modal('#searchModal');
 
 const closeSearchModal = () => {
     myModal.show = 0
